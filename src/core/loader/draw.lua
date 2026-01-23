66fa4f79e874925a344e73410af17de78e470b10
Pandaman331 <kozekmc.1234@gmail.com>
2026-01-23 14:29:13 +0100

    Made working state manager with preloading option.

--- src/core/loader/draw.lua
+++ src/core/loader/draw.lua
@@ -1,3 +1,7 @@
 return function()
   -- print("[OK INIT.draw]\nsrc/core/loader/init/draw.lua\nDraw function started.")
+  local stateManager = Loader.getModule({ "stateManager" })
+  if stateManager and stateManager.currentState and stateManager.currentState.draw then
+    stateManager.currentState.draw()
+  end
 end
